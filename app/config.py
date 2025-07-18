19e6d5bdf9bf12d3574edff93866469f33c9a6e4
darkoddish <thjones29@gmail.com>
2025-07-18 11:46:14 -0400

    gitignore cleanup

--- app/config.py
+++
@@ -1,8 +0,0 @@
-# Configuration file to store sensitive information
-
-RTSP_USERNAME = "rpi"
-RTSP_PASSWORD = "RPI_bw_99"
-RTSP_IP = "192.168.1.6"
-RTSP_PORT = "554"
-RTSP_STREAM_PATH_SUB = "/h264Preview_01_sub"
-RTSP_STREAM_PATH_MAIN = "/h264Preview_01_main"
